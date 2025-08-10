00689e87ddb392aa5adaaa230951de2d1ccce68d
ChanchalSinghRathore <chanchalrathore111003@gmail.com>
2025-08-10 23:28:35 +0530

    Enhance video controller with error handling and update functionality; validate video ID and ownership for update, delete, and toggle publish actions

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -55,28 +55,132 @@ const publishAVideo = asyncHandler(async (req, res) => {
 
      res
      .status(200)
-     .json(ApiResponse.success("Video Published", video))
+     .json(new ApiResponse(200,"Video Published", video))
     
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: get video by id
+    if (!mongoose.Types.ObjectId.isValid(videoId)) {
+        throw new ApiError(400, "Invalid video ID")
+    }
+    const video = await Video.findById(videoId)
+    if (!video) {
+        throw new ApiError(404, "Video not found");
+    }
+
+    res
+    .status(200)
+    .json(new ApiResponse(200, video, "video fetched successfully"))
+    
+
+
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: update video details like title, description, thumbnail
+    if (!mongoose.Types.ObjectId.isValid(videoId)) {
+        throw new ApiError(400, "Invalid video ID")
+    }
 
+    if (video.owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, "You are not authorized to update this video");
+    }
+
+    const {updatedTitle, updatedDescription, updatedThumbnail} = req.body
+
+    if (!updatedTitle && !updatedDescription && !updatedThumbnail) {
+        throw new ApiError(400, "action required, no updated data provided")
+    }
+
+    const updatedThumbnailLocalPath = req.files?.thumbnail?.[0]?.path
+
+    if (!updatedThumbnailLocalPath) {
+        throw new ApiError(400, "updated thumbnail not found or uploaded by user")
+    }
+
+    const thumbnail = await uploadOnCloudinary(updatedThumbnailLocalPath)
+
+    if (!thumbnail.url) {
+        throw new ApiError(500, "thumbnail could not be uploaded on cloudinary")
+    }
+
+    const video = await Video.findByIdAndUpdate(
+        videoId,
+        {
+            $set:{
+                title: updatedTitle,
+                description: updatedDescription,
+                thumbnail: thumbnail.url
+            }
+        },
+        {new: true}
+    )
+
+    res
+    .status(200)
+    .json(new ApiResponse(200, video, "All details updated successfully"))
+    
 })
 
 const deleteVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: delete video
+
+    if (!mongoose.Types.ObjectId.isValid(videoId)) {
+        throw new ApiError(400, "Invalid video ID")
+    }
+
+    const video = await Video.findById(videoId)
+
+    if (!video) { 
+        throw new ApiError(404, "Video not found")
+    }
+
+    if(video.owner.toString()!==req.user?._id.toString()){
+      throw new ApiError(403,"You are not authorized to delete this video")
+   }
+    
+    Video.findByIdAndDelete(videoId)
+
+    res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Video deleted successfully"))
+
 })
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
     const { videoId } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(videoId)) {
+        throw new ApiError(400, "Invalid video ID")
+    }
+
+    const video = await Video.findById(videoId)
+
+    if (!video) {
+        throw new ApiError(404, "video not found")
+    }
+
+    if (video.owner.toString() !== req.user?._id.toString()) {
+      throw new ApiError(403, "You are not authorized to toggle publish status");
+    }
+
+    const VideoToggleStatus = await Video.findByIdAndUpdate(
+        videoId,
+        { isPublished: !video.isPublished },
+        { new: true, runValidators: false } // new:true returns updated doc
+    );
+
+    const message = VideoToggleStatus.isPublished
+        ? "Video published successfully"
+        : "Video unpublished successfully";
+
+    res
+    .status(200)
+    .json(new ApiResponse(200, VideoToggleStatus, message))
 })
 
 export {
